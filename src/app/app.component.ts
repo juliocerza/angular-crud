9b30c8561664e396b2091905bc4a4aa3b8ec55b0
Cesar Rocha <cesar_fisico@outlook.com>
2023-11-27 10:38:47 -0300

    Angular crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
